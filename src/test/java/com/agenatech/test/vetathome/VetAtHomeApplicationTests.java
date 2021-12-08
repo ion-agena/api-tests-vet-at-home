26aed76b46e60f6141cfdb5d1ffea1b08499770d
Ion Ionets <ion.ionets@qvantel.com>
2021-12-08 20:29:37 +0800

    refactoring

--- src/test/java/com/agenatech/test/vetathome/VetAtHomeApplicationTests.java
+++ src/test/java/com/agenatech/test/vetathome/VetAtHomeApplicationTests.java
@@ -19,6 +19,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -104,7 +105,7 @@ class VetAtHomeApplicationTests {
 
 		List<UserProfile> owners = gatewayService.searchProfilesByPetIds(Arrays.asList(petId)).get_embedded().getProfiles();
 
-		assertTrue(isDefaultUserPresentInTheList(owners));
+		assertTrue(isUserPresentInTheList(DEFAULT_USER_ID, owners));
 	}
 
 	@Test
@@ -132,13 +133,49 @@ class VetAtHomeApplicationTests {
 
 		List<UserProfile> owners = gatewayService.searchProfilesByPetIds(Arrays.asList(petId)).get_embedded().getProfiles();
 
-		assertTrue(isDefaultUserPresentInTheList(owners));
+		assertTrue(isUserPresentInTheList(DEFAULT_USER_ID, owners));
 	}
 
-	private boolean isDefaultUserPresentInTheList(List<UserProfile> owners){
+
+	@Test
+	public void linkFalse(){
+		PetProfile generatedPetProfile = dataManager.generatePetProfile();
+		PetProfile retrievedPet = gatewayService.saveAndLink(generatedPetProfile);
+		log.debug("-------retrievedPet {}", retrievedPet);
+
+		UserProfile generatedProfile = dataManager.generateUserProfile();
+		log.debug("-------generated profile {}", generatedProfile);
+
+		UserProfile retrievedProfile = gatewayService.putProfile(generatedProfile.getAvatarUrl(), generatedProfile);
+		log.debug("-------retrieved profile {}", retrievedProfile);
+
+		UserPetLink newLink = UserPetLink.builder()
+				.userLink(uriUtils.getSelfLinkFromUser(retrievedProfile))
+				.petLink(uriUtils.getPetLinkFromPet(retrievedPet))
+				.build();
+
+
+		gatewayService.link(newLink);
+
+		String petId = uriUtils.getIdFromLink(uriUtils.getPetLinkFromPet(retrievedPet));
+		log.debug("-------petId {}", petId);
+
+//		UserProfile fakeProfile = dataManager.generateUserProfile();
+//		fakeProfile.set_links(HalLinks.builder().self(new HashMap<>() {{
+//			put("href", "http://test/" + fakeProfile.getAvatarUrl());
+//		}}).build());
+//
+//		List<UserProfile> fakeOwners = Arrays.asList(fakeProfile);
+		List<UserProfile> owners = gatewayService.searchProfilesByPetIds(Arrays.asList(petId)).get_embedded().getProfiles();
+
+
+		assertFalse(isUserPresentInTheList(UUID.randomUUID().toString(), owners));
+	}
+
+	private boolean isUserPresentInTheList(String userId, List<UserProfile> owners){
 		return owners.stream().map(owner -> uriUtils.getSelfLinkFromUser(owner) )
 				.map(link -> uriUtils.getIdFromLink(link)).collect(Collectors.toList())
-				.contains(DEFAULT_USER_ID);
+				.contains(userId);
 	}
 
 
